3a05bdf1ab2b90f0f2dd1273579b1d5956b9ae93
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-25 13:33:06 +0900

    本の登録機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @books = Book.includes(:user).order("created_at ASC")
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :publisher, :image_url).merge(user_id: current_user.id)
   end
 end
