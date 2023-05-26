34c7b6ba62cef41be60bf648018ed535155dd60d
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-26 11:11:10 +0900

    詳細表示機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    @book = Book.find(params[:id])
+    # if user_signed_in?
+    #   @loan = Loan.find_by(book_id: @book.id, user_id: current_user.id)
+    # end
+  end
+
   private
 
   def book_params
