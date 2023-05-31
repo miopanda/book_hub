2b910d3dde7d5247b7427b04f78a2a3637bea5f5
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-31 18:57:43 +0900

    検索ボタン等修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,8 +6,8 @@ class Book < ApplicationRecord
   validates :title, presence: true
 
   def self.search(search)
-    if search != ""
-      Book.where('title LIKE(?)', "%#{search}%")
+    if search.present?
+      Book.where('LOWER(title) LIKE ?', "%#{search.downcase}%")
     else
       Book.all
     end
